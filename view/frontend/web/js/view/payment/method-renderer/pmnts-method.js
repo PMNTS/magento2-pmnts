9a60e4ad5b06e0d7e87f40485ad3ca9d8683ccbd
Matthew Savage <matt@amasses.net>
2016-08-02 22:01:21 +1000

    Clean up, renaming etc

---
+++ view/frontend/web/js/view/payment/method-renderer/pmnts-method.js
@@ -0,0 +1,39 @@
+/**
+ * PMNTS_Gateway Magento JS component
+ *
+ * @category    PMNTS
+ * @package     PMNTS_Gateway
+ * @copyright   PMNTS (http://PMNTS.io)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'Magento_Payment/js/view/payment/cc-form',
+        'jquery',
+        'Magento_Payment/js/model/credit-card-validation/validator'
+    ],
+    function (Component, $) {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                template: 'pmnts_gateway/payment/pmnts-form'
+            },
+
+            getCode: function() {
+                return 'pmnts_gateway';
+            },
+
+            isActive: function() {
+                return true;
+            },
+
+            validate: function() {
+                var $form = $('#' + this.getCode() + '-form');
+                return $form.validation() && $form.validation('isValid');
+            }
+        });
+    }
+);
---
+++ view/frontend/web/js/view/payment/pmnts-payments.js
@@ -0,0 +1,30 @@
+/**
+ * PMNTS_Gateway Magento JS component
+ *
+ * @category    PMNTS
+ * @package     PMNTS_Gateway
+ * @copyright   PMNTS (http://PMNTS.io)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
+        'use strict';
+        rendererList.push(
+            {
+                type: 'pmnts_gateway',
+                component: 'pmnts_gateway/js/view/payment/method-renderer/Gateway-method'
+            }
+        );
+        /** Add view logic here if needed */
+        return Component.extend({});
+    }
+);
