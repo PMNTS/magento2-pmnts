137e4cec8265e81cdca87139f037ccd171541b27
Matthew Savage <matt@amasses.net>
2016-08-07 20:34:30 +1000

    Updated provider to return if iframe is enabled or not

--- Model/Config/PmntsConfigProvider.php
+++ Model/Config/PmntsConfigProvider.php
@@ -6,6 +6,26 @@ use Magento\Checkout\Model\ConfigProviderInterface;
 
 class PmntsConfigProvider implements ConfigProviderInterface
 {
+
+    /**
+    * @var string[]
+    */
+    protected $methodCode = \PMNTS\Gateway\Model\Payment::CODE;
+    /**
+    * @var \Foggyline\Paybox\Model\Paybox
+    */
+    protected $method;
+
+    /**
+     * @param \Magento\Payment\Helper\Data $paymentHelper
+     */
+    public function __construct(
+        \Magento\Payment\Helper\Data $paymentHelper
+    ) {
+        $this->method = $paymentHelper->getMethodInstance($this->methodCode);
+    }
+
+
     /**
      * {@inheritdoc}
      */
@@ -22,26 +42,30 @@ class PmntsConfigProvider implements ConfigProviderInterface
     }
 
     private function getConfigValue($key) {
-      return $this->_scopeConfig->getValue("payment/pmnts_gateway/$key");
+      return $this->method->getValue("payment/pmnts_gateway/$key");
+    }
+
+    private function getIframeEnabled() {
+      return (bool)$this->getConfigValue("iframe_tokenization");
     }
 
     private function getIframeSrc() {
-        $is_sandbox = $this->getConfigValue("iframe_tokenization");
-        $username = $this->getConfigValue("username")
+        $is_sandbox = $this->getConfigValue("sandbox_mode");
+        $username = $this->getConfigValue("username");
         $shared_secret = $this->getConfigValue("shared_secret");
         $nonce = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 5);
         $hash_payload = "$nonce:100.25:AUD";
         $hash = hash_hmac ("md5", $hash_payload, $shared_secret);
 
-        $url = "https://paynow.pmnts.io/v2/$username/$nonce/AUD/1.0/$hash?show_extras=false&iframe=false&paypal=false&show_extras=false";
+        $url = "https://paynow.pmnts.io/v2/{$username}/{$nonce}/AUD/1.0/{$hash}?show_extras=false&iframe=false&paypal=false&show_extras=false";
         // https://paynow-sandbox.pmnts.io/
         if($is_sandbox) {
-            $url = "https://paynow-sandbox.pmnts.io/v2/$username/$nonce/AUD/1.0/$hash?show_extras=false&iframe=false&paypal=false&show_extras=false";
+            $url = "https://paynow-sandbox.pmnts.io/v2/{$username}/{$nonce}/AUD/1.0/{$hash}?show_extras=false&iframe=false&paypal=false&show_extras=false";
         }
 
         // If CSS URL is set, generate signature, add to iframe URL
+        $css_url = $this->getConfigValue("iframe_css");
         if (strlen($css_url) > 0) {
-          $css_url = $this->getConfigValue("iframe_css");
           $css_signature = hash_hmac("md5", $css_url, $shared_secret);
           $url = $url . "&css=$css_url&css_signature=$css_signature";
         }
